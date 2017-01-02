fc9e95b036323b9c190f48e0a24f243790b3b66e
AKHEEL K M <akheelkm@AKHEELs-MacBook-Pro.local>
2017-01-02 11:41:31 +0530

    extract features and chroma from input image

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import brisk
-import chromaEx as CE
+import chroma_ex as ce
+import helper as hp
+import numpy as np
 
-matrix =  brisk.get_features('col.jpg')
-chroma = CE.extractA('col.jpg')
+extracted_features =  brisk.get_features('col.jpg')
+input_x = hp.to_single_dimensional_array(extracted_features)
 
-print chroma
+print "input_x :",np.array(input_x)
+print "len of x",len(input_x)
 
-print 'length of matrix : ',len(matrix)
-print 'breadth of matrix : ',len(matrix[0])
+chroma = ce.extract_A('col.jpg')
+input_y = hp.to_single_dimensional_array(chroma)
+print "input_y :",np.array(input_y)
+print "len of input_y",len(input_y)
