3de6ef2d175b3bfe649806f5655f099034a520f2
goks <gokulav2@gmail.com>
2017-01-03 22:37:09 +0530

    Separate train and test pre-works

--- pre_works_test.py
+++ pre_works_test.py
@@ -4,7 +4,6 @@ from glob import glob
 # import os
 from os.path import exists, join, basename, splitext
 import numpy
-from chroma_ex import extract_A
 
 
 EXTENSIONS = [".jpg", ".bmp"]
@@ -32,43 +31,19 @@ def get_imgfiles(path):						#gets image from the path
     print all_files
     return all_files
 
-def process_brisk(imagename, resultname="temp.brisk"):
-	""" process an image and save the results in a .key ascii file"""
-	print("(brisk)working on " + imagename)
-	# run extraction command
-	keypoints = brisk.get_features( imagename, resultname)
-	print( str(len(keypoints)) + " keypoints found.")
-	return
-
-def process_chroma(imagename, resultname="temp.chroma"):
-	""" process an image and save the results in a .key ascii file"""
-	print("(chroma)working on " + imagename)
-	# run extraction command
-	extract_A( imagename, resultname)
-	return
-
 def process_images(input_files):
     print "BEGIN EXTRACTION"
     brisk_features_arr = []
-    chroma_features_arr = []
     for i, fname in enumerate(input_files):
         brisk_fname = fname + '.brisk'
-        chroma_fname = fname + '.chroma'
         if exists(brisk_fname) == False:
             print "step i: calculating brisk for", fname
             process_brisk(fname, brisk_fname)
-        if exists(chroma_fname) == False:
-            print "step ii: calculating chroma for", fname
-            process_chroma(fname, chroma_fname)
         print "gathering brisk for", fname
         """ HAVE TO TEST THIS PART """
         descriptors = numpy.load(brisk_fname)
-        print "gathering chroma for", fname
-        chroma = numpy.load(chroma_fname)
-        print chroma.shape
         brisk_features_arr.append(descriptors)
-        chroma_features_arr.append(chroma)
-    return brisk_features_arr, chroma_features_arr
+    return brisk_features_arr
 
 def begin(datasetpath = DATASETPATH):
 	print "---------------------"
